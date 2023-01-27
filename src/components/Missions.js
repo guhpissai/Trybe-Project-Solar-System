eb05be5b4978f1e3bc234d4aa67867cdcf309dfc
guhpissai <gustavopissai3d@gmail.com>
2023-01-27 13:05:31 -0300

    Ajustando requisito 8

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -7,7 +7,7 @@ class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        <Title />
+        <Title headline={'Missões'}/>
         {missions.map((mission) => (
           <MissionCard
             key={ mission.name }
