4bb75146536ddee51f89f5064bce17ffa3d3484f
guhpissai <gustavopissai3d@gmail.com>
2023-01-27 15:32:23 -0300

    Ajustando erros lint

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -7,7 +7,7 @@ class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        <Title headline={'Missões'}/>
+        <Title headline="Missões" />
         {missions.map((mission) => (
           <MissionCard
             key={ mission.name }
