166f8ce9ba4eed6cafcd3507332b2bd7e5ff4703
guhpissai <gustavopissai3d@gmail.com>
2023-03-01 12:07:59 -0300

    Adicionando icones

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -8,11 +8,11 @@ class MissionCard extends React.Component {
       <div className='missions' data-testid="mission-card">
         <h2 data-testid="mission-name">{name}</h2>
         <div className='mission-info'>
-          <div className='info-1'>
-            <p data-testid="mission-year">{year}</p>
-            <p data-testid="mission-country">{country}</p>
+          <div className='info-card'>
+            <p className='info-1' data-testid="mission-year">{year}</p>
+            <p className='info-2' data-testid="mission-country">{country}</p>
           </div>
-            <p className='info-2' data-testid="mission-destination">{destination}</p>
+            <p className='info-3' data-testid="mission-destination">{destination}</p>
         </div>
     </div>
     );
